798fb142907738333eaa77d571d9a00071dfdb4b
leomosley <leo.mosley@network.elancoah.com>
2026-05-09 03:26:23 +0100

    fix(ci): remove invalid tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -13,7 +13,6 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_s3_checksum            = true
-    use_path_style_endpoint     = true
   }
 
   required_providers {
